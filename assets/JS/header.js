d0282d7a60d591209aad0adae5f0a3e68fb03462
TungVan57 <thientung57203@gmail.com>
2024-01-09 21:02:59 +0700

    update list phone

--- assets/JS/header.js
+++ assets/JS/header.js
@@ -28,4 +28,64 @@ const mega_menu_Res = document.querySelector(".toggle .mega-menu");
 toggleMobile.addEventListener("click", () => {
   mega_menu_Res.classList.toggle("active");
   siteInnerAfter.classList.toggle("active");
+  sectionFooterAfter.classList.toggle("active");
 });
+// Tạo một hàm để xử lý sự kiện resize
+function handleResize() {
+  // Lấy chiều rộng màn hình
+  let windowWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  // In ra console để kiểm tra
+  if (windowWidth >= 1024) {
+    if (mega_menu_Res.classList.contains("active")) {
+      mega_menu_Res.classList.remove("active");
+    }
+    if (
+      sectionFooterAfter.classList.contains("active") &&
+      (!mega_menu_nav.classList.contains("active") ||
+        !mega_menu_nav_fix.classList.contains("active"))
+    ) {
+      sectionFooterAfter.classList.remove("active");
+    }
+    if (
+      siteInnerAfter.classList.contains("active") &&
+      (!mega_menu_nav.classList.contains("active") ||
+        !mega_menu_nav_fix.classList.contains("active"))
+    ) {
+      siteInnerAfter.classList.remove("active");
+    }
+  }
+  if (windowWidth <= 1024) {
+    if (
+      sectionFooterAfter.classList.contains("active") &&
+      (mega_menu_nav.classList.contains("active") ||
+        mega_menu_nav_fix.classList.contains("active"))
+    ) {
+      sectionFooterAfter.classList.remove("active");
+    }
+    if (
+      siteInnerAfter.classList.contains("active") &&
+      (mega_menu_nav.classList.contains("active") ||
+        mega_menu_nav_fix.classList.contains("active"))
+    ) {
+      siteInnerAfter.classList.remove("active");
+    }
+    if (mega_menu_nav.classList.contains("active")) {
+      mega_menu_nav.classList.remove("active");
+    }
+    if (mega_menu_nav_fix.classList.contains("active")) {
+      mega_menu_nav_fix.classList.remove("active");
+    }
+  }
+
+  // Thực hiện các thao tác khác dựa trên chiều rộng màn hình nếu cần
+}
+
+// Đăng ký hàm handleResize để được gọi khi cửa sổ được resize
+window.addEventListener("resize", handleResize);
+
+// Gọi hàm handleResize một lần khi trang web được tải lần đầu tiên
+handleResize();
--- assets/JS/main.mjs
+++ assets/JS/main.mjs
@@ -60,7 +60,7 @@ btnTop.addEventListener("click", () => {
 });
 
 //buildProduct
-const listItem = document.querySelector("#list-product");
+const listItem = document.querySelector("#list-product-phone");
 
 const buildItemTemplate = (item) => {
   const templateItem = document.querySelector("#itemTemplate");
@@ -91,6 +91,7 @@ const buildItemTemplate = (item) => {
 };
 
 const loading = document.querySelector("#loading");
+let listProductPhone = [];
 const init = async () => {
   try {
     {
@@ -103,45 +104,139 @@ const init = async () => {
           if (index >= 6) {
             const itemElement = buildItemTemplate(item);
             itemElement.classList.add("active");
+            listItem.appendChild(itemElement);
           } else {
             const itemElement = buildItemTemplate(item);
             listItem.appendChild(itemElement);
           }
         });
       }
+      listProductPhone = [
+        ...document.querySelectorAll("#list-product-phone .owl-item"),
+      ];
+      console.log(listProductPhone);
     }
   } catch (e) {
     console.log("Main", e);
   }
 };
 init();
+const btnPreList = document.querySelector(".box-heading #prevItem");
+const btnNextList = document.querySelector(".box-heading #nextItem");
 
-// const btnPreList = document.querySelector(".box-heading #prevItem");
-// const btnNextList = document.querySelector(".box-heading #nextItem");
-// console.log(btnPreList);
-// const listProduct = document.querySelectorAll('#list-product .owl-item');
-// console.log(listProduct[0]);
+// Lưu đối tượng vào Local Storage
+let myFlag = { flagFirst: 0, flagNow: 5 };
+let dataFlag = JSON.parse(localStorage.getItem("myFlag")) || myFlag;
 
-// let activeIndexList = 6;
-// function changeSlideList(page) {
-//   console.log(listProduct[activeIndex-6]);
-//   listProduct[activeIndex-6].classList.remove("active");
-//    activeIndexList += page;
+localStorage.setItem("myFlag", JSON.stringify(myFlag));
 
-//   if (activeIndex === -1) {
-//     activeIndexList = listProduct.length - 1;
-//   }
-//   if (activeIndex === listProduct.length) {
-//     activeIndexList = 0;
-//   }
-//   listProduct[activeIndex+6].classList.add("active");
-//   console.log(activeIndexList);
-// }
-// btnPreList.addEventListener("click", function () {
-//   console.log('object');
-//   // changeSlideList(-1);
-// });
-// btnNextList.addEventListener("click", function () {
-//   console.log('object');
-//   changeSlideList(1);
-// });
+function changeSlideList(page) {
+  // console.log(listProductPhone[1]);
+  if (page === 1 && dataFlag.flagNow !== listProductPhone.length - 1) {
+    listProductPhone[dataFlag.flagNow + page].classList.remove("active");
+    dataFlag.flagNow++;
+    listProductPhone[dataFlag.flagFirst].classList.add("active");
+    dataFlag.flagFirst++;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  if (page === -1 && dataFlag.flagFirst !== 0) {
+    console.log(listProductPhone[5 + page]);
+    listProductPhone[dataFlag.flagFirst + page].classList.remove("active");
+    dataFlag.flagFirst--;
+    listProductPhone[dataFlag.flagNow].classList.add("active");
+    dataFlag.flagNow--;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+}
+btnPreList.addEventListener("click", function () {
+  changeSlideList(-1);
+});
+btnNextList.addEventListener("click", function () {
+  changeSlideList(1);
+});
+
+function handleResize() {
+  // Lấy chiều rộng màn hình
+  let windowWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  // In ra console để kiểm tra
+  if (windowWidth > 1280) {
+    listProductPhone.forEach((element, index) => {
+      if (index <= dataFlag.flagFirst + 5 && index >= dataFlag.flagFirst) {
+        element.classList.remove("active");
+      } else {
+        element.classList.add("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 5;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  if (windowWidth <= 1280 && windowWidth > 1024) {
+    listProductPhone.forEach((element, index) => {
+      if (index > dataFlag.flagFirst + 4) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 4;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  if (windowWidth <= 1024 && windowWidth > 768) {
+    listProductPhone.forEach((element, index) => {
+      if (index > dataFlag.flagFirst + 3) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 3;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+
+  if (windowWidth <= 768 && windowWidth > 640) {
+    listProductPhone.forEach((element, index) => {
+      if (index > dataFlag.flagFirst + 2) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 2;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  if (windowWidth <= 640 && windowWidth > 480) {
+    listProductPhone.forEach((element, index) => {
+      if (index > dataFlag.flagFirst + 1) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 1;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  if (windowWidth <= 480) {
+    listProductPhone.forEach((element, index) => {
+      if (index > dataFlag.flagFirst + 0) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    });
+    dataFlag.flagNow = dataFlag.flagFirst + 1;
+    localStorage.setItem("myFlag", JSON.stringify(dataFlag));
+  }
+  console.log("First", flagFirst);
+  console.log("now", flagNow);
+  // Thực hiện các thao tác khác dựa trên chiều rộng màn hình nếu cần
+}
+
+// Đăng ký hàm handleResize để được gọi khi cửa sổ được resize
+window.addEventListener("resize", handleResize);
+
+// Gọi hàm handleResize một lần khi trang web được tải lần đầu tiên
+handleResize();
